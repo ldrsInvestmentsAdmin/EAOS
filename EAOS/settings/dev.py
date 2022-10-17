7568b555f060ae5b089e3575c6cb7f4fcae1bfb2
Zachary John <admin@ldrsinvestments.ca>
2022-10-17 11:46:37 -0400

    Forgot to import base settings file

--- EAOS/settings/dev.py
+++ EAOS/settings/dev.py
@@ -1,3 +1,5 @@
+from .base import *
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
--- EAOS/settings/prod.py
+++ EAOS/settings/prod.py
@@ -1,4 +1,5 @@
 import os
+from .base import *
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
