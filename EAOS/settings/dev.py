8a84d7a39c1a97fc944cc7648fffc450876405f8
Zachary John <admin@ldrsinvestments.ca>
2022-10-17 12:05:59 -0400

    Added PostgreSQL database. Added database information in settings file. Installed psycopg2

--- EAOS/settings/dev.py
+++ EAOS/settings/dev.py
@@ -8,3 +8,17 @@ SECRET_KEY = 'django-insecure-1l^gr*i8+4p+^v4-c=%^3k-k9n2k^fy-l90+!%*^#avmug3_u#
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
+
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER':'postgres',
+        'PASSWORD':'5r43UDwwtcevl7DLLhG7',
+        'HOST':'containers-us-west-29.railway.app',
+        'PORT':'7808'
+    }
+}
--- EAOS/settings/prod.py
+++ EAOS/settings/prod.py
@@ -10,3 +10,17 @@ SECRET_KEY = os.environ['SECRET_KEY']
 DEBUG = True
 
 ALLOWED_HOSTS = ['ldrs-eaos-production.up.railway.app']
+
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ['PGDATABASE'],
+        'USER': os.environ['PGUSER'],
+        'PASSWORD': os.environ['PGPASSWORD'],
+        'HOST': os.environ['PGHOST'],
+        'PORT': os.environ['PGPORT']
+    }
+}
